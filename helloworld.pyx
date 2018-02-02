682db67ab7d95b21f5b9108c6d79d6781a5d5119
yutiansut <yutiansut@qq.com>
2018-02-02 19:13:33 +0800

    #commit

--- helloworld.pyx
+++ helloworld.pyx
@@ -1,4 +1,17 @@
 cdef extern from"stdio.h":
     extern int printf(const char *format, ...) 
-def SayHello():
+    
+cpdef SayHello():
     printf("hello,world\n")
+
+def f(double x):
+    return x**2-x
+
+def integrate_f(double a, double b, int N):
+    cdef int i
+    cdef double s, dx
+    s = 0
+    dx = (b-a)/N
+    for i in range(N):
+        s += f(a+i*dx)
+    return s * dx
